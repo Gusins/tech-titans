99ddd17350339f3bceab015fb172a60027743fa3
Kuzkoviy <72725653+Kuzkoviy@users.noreply.github.com>
2024-06-02 16:24:19 +0300

    faq-section almost done

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,6 +1,43 @@
-// TODO: зробити функцію та екпортувати її в main.js 
-import Accordion from 'accordion-js';
-import 'accordion-js/dist/accordion.min.css';
+document.addEventListener('DOMContentLoaded', function () {
+    const triggers = document.querySelectorAll('.ac-header');
+    const panels = document.querySelectorAll('.ac-panel');
+    const svgs = document.querySelectorAll('.ac-svg');
+    const items = document.querySelectorAll('.ac');
 
+    // Установка початкового стану першого елементу акордеону
+    items[0].classList.add('open');
+    panels[0].classList.add('open');
+    svgs[0].classList.add('rotate');
+    triggers[0].style.backgroundColor = 'white'; // Змінили колір фону на білий
+    panels[0].style.backgroundColor = 'rgba(0, 176, 104, 0.20)'; // Змінили колір фону 
 
+    triggers.forEach((trigger, index) => {
+      trigger.addEventListener('click', function () {
+        const isOpen = items[index].classList.contains('open');
 
+        // Закриття всіх елементів
+        items.forEach(item => {
+          item.classList.remove('open');
+        });
+        panels.forEach(p => {
+          p.classList.remove('open');
+          p.style.backgroundColor = 'transparent';
+        });
+        svgs.forEach(svg => {
+          svg.classList.remove('rotate');
+        });
+        triggers.forEach(t => {
+          t.style.backgroundColor = 'white'; // Змінили колір фону на білий
+        });
+
+        // Відкриття або закриття поточного елемента
+        if (!isOpen) {
+          items[index].classList.add('open');
+          panels[index].classList.add('open');
+          svgs[index].classList.add('rotate');
+          triggers[index].style.backgroundColor = 'white'; // Змінили колір фону на білий
+          panels[index].style.backgroundColor = 'rgba(0, 176, 104, 0.20)'; // Змінили колір фону
+        }
+      });
+    });
+});
--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
 // import * as ns from "./js/reviews";
 import * as modalWindow from "./js/modal";
 import * as workTogether from "./js/work-together.js";
+
+import './js/faq.js'
