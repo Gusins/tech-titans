5a74255cf4d97928053da9edb65a3dc761656c41
Malakin1116 <130695935+Malakin1116@users.noreply.github.com>
2024-06-02 15:04:34 +0300

    cleaned_out_the_trash

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,29 +1,3 @@
-// export const CHANGE_HTML_FOR_HERO = () => {
-//     const HERO_MAIN_DIV = document.querySelector('.hero-main-div');
-//     const EXISTING_ELEMENT = document.querySelector('.hero-main-open');
-
-//     if (EXISTING_ELEMENT) {
-//         EXISTING_ELEMENT.remove();
-//         HERO_MAIN_DIV.style.display = "block";
-//     } else {
-//         let NEW_ELEMENT = document.createElement('div');
-//         NEW_ELEMENT.className = 'hero-main-open';
-//         NEW_ELEMENT.innerHTML = `
-//              <div class="menu-items-div">
-//                 <button id="closeModalBtn">☰</button>
-//                 <ul class="menu-items-opens">
-//                      <li class="header-menu-li"><a href="#about-me">About me</a></li>
-//                     <li class="hero-social-li"><a href="https://www.facebook.com/goITclub/">Projects</a></li>
-//                     <li class="hero-social-li"><a href="#">Contacts</a></li>
-//                 </ul>
-//             </div>
-//         `;
-
-//         HERO_MAIN_DIV.style.display = "none"; 
-//         HERO_MAIN_DIV.parentNode.insertBefore(NEW_ELEMENT, HERO_MAIN_DIV); 
-//     }
-// };
-
 
 export const CHANGE_HTML_FOR_HERO = () => {
     const HERO_MAIN_DIV = document.querySelector('.hero-main-div');
