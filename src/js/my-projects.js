bbd902130c4457cf312a3812c9d96d71e44ed206
Oleksandr Volokhov <Flatroniym@gmail.com>
2024-06-03 12:48:02 +0200

    fixing bugs with pictures on github pages

--- src/js/my-projects.js
+++ src/js/my-projects.js
@@ -3,8 +3,8 @@
 const projects = [
 	{
 		img: {
-			src: 'img/my-projects/wallet.jpg',
-			srcset: 'img/my-projects/wallet.jpg 1x, img/my-projects/wallet-2x.jpg 2x',
+			src: 'src/img/my-projects/wallet.jpg',
+			srcset: 'src/img/my-projects/wallet.jpg 1x, src/img/my-projects/wallet-2x.jpg 2x',
 			alt: 'wallet webservice'
 		},
 		stack: 'React, JavaScript, Node JS, Git',
@@ -13,8 +13,8 @@ const projects = [
 	},
 	{
 		img: {
-			src: 'img/my-projects/green.jpg',
-			srcset: 'img/my-projects/green.jpg 1x, img/my-projects/green-2x.jpg 2x',
+			src: 'src/img/my-projects/green.jpg',
+			srcset: 'src/img/my-projects/green.jpg 1x, src/img/my-projects/green-2x.jpg 2x',
 			alt: 'green harvest webservice'
 		},
 		stack: 'React, JavaScript, Node JS, Git',
@@ -23,8 +23,8 @@ const projects = [
 	},
 	{
 		img: {
-			src: 'img/my-projects/english.jpg',
-			srcset: 'img/my-projects/english.jpg 1x, img/my-projects/english-2x.jpg 2x',
+			src: 'src/img/my-projects/english.jpg',
+			srcset: 'src/img/my-projects/english.jpg 1x, src/img/my-projects/english-2x.jpg 2x',
 			alt: 'English excellence webservice'
 		},
 		stack: 'React, JavaScript, Node JS, Git',
@@ -33,8 +33,8 @@ const projects = [
 	},
 	{
 		img: {
-			src: 'img/my-projects/power.jpg',
-			srcset: 'img/my-projects/power.jpg 1x, img/my-projects/power-2x.jpg 2x',
+			src: 'src/img/my-projects/power.jpg',
+			srcset: 'src/img/my-projects/power.jpg 1x, src/img/my-projects/power-2x.jpg 2x',
 			alt: 'power pulse webservice'
 		},
 		stack: 'React, JavaScript, Node JS, Git',
@@ -43,8 +43,8 @@ const projects = [
 	},
 	{
 		img: {
-			src: 'img/my-projects/mimino.jpg',
-			srcset: 'img/my-projects/mimino.jpg 1x, img/my-projects/mimino-2x.jpg 2x',
+			src: 'src/img/my-projects/mimino.jpg',
+			srcset: 'src/img/my-projects/mimino.jpg 1x, src/img/my-projects/mimino-2x.jpg 2x',
 			alt: 'mimino website'
 		},
 		stack: 'React, JavaScript, Node JS, Git',
@@ -53,8 +53,8 @@ const projects = [
 	},
 	{
 		img: {
-			src: 'img/my-projects/vyshyvanka.jpg',
-			srcset: 'img/my-projects/vyshyvanka.jpg 1x, img/my-projects/vyshyvanka-2x.jpg 2x',
+			src: 'src/img/my-projects/vyshyvanka.jpg',
+			srcset: 'src/img/my-projects/vyshyvanka.jpg 1x, src/img/my-projects/vyshyvanka-2x.jpg 2x',
 			alt: 'vyshyvanka vibes Landing Page'
 		},
 		stack: 'React, JavaScript, Node JS, Git',
@@ -63,8 +63,8 @@ const projects = [
 	},
 	{
 		img: {
-			src: 'img/my-projects/chego.jpg',
-			srcset: 'img/my-projects/chego.jpg 1x, img/my-projects/chego-2x.jpg 2x',
+			src: 'src/img/my-projects/chego.jpg',
+			srcset: 'src/img/my-projects/chego.jpg 1x, src/img/my-projects/chego-2x.jpg 2x',
 			alt: 'chego jewelry website'
 		},
 		stack: 'React, JavaScript, Node JS, Git',
@@ -73,8 +73,8 @@ const projects = [
 	},
 	{
 		img: {
-			src: 'img/my-projects/energy.jpg',
-			srcset: 'img/my-projects/energy.jpg 1x, img/my-projects/energy-2x.jpg 2x',
+			src: 'src/img/my-projects/energy.jpg',
+			srcset: 'src/img/my-projects/energy.jpg 1x, src/img/my-projects/energy-2x.jpg 2x',
 			alt: 'energy flow webservice'
 		},
 		stack: 'React, JavaScript, Node JS, Git',
@@ -83,8 +83,8 @@ const projects = [
 	},
 	{
 		img: {
-			src: 'img/my-projects/fruit.jpg',
-			srcset: 'img/my-projects/fruit.jpg 1x, img/my-projects/fruit-2x.jpg 2x',
+			src: 'src/img/my-projects/fruit.jpg',
+			srcset: 'src/img/my-projects/fruit.jpg 1x, src/img/my-projects/fruit-2x.jpg 2x',
 			alt: 'fruitbox online store'
 		},
 		stack: 'React, JavaScript, Node JS, Git',
@@ -93,8 +93,8 @@ const projects = [
 	},
 	{
 		img: {
-			src: 'img/my-projects/starlight.jpg',
-			srcset: 'img/my-projects/starlight.jpg 1x, img/my-projects/starlight-2x.jpg 2x',
+			src: 'src/img/my-projects/starlight.jpg',
+			srcset: 'src/img/my-projects/starlight.jpg 1x, src/img/my-projects/starlight-2x.jpg 2x',
 			alt: 'starlight studio landing page'
 		},
 		stack: 'React, JavaScript, Node JS, Git',
@@ -120,7 +120,7 @@ const createProjectMarkup = function (project) {
       <a class="projects-card-link" href="${project.link}" target="_blank">
         <span class="projects-card-link-span">VISIT</span>
         <svg class="projects-card-link-svg" width="24" height="24">
-          <use href="img/svg/spriteForEachOpt.svg#MyProjectVector"></use>
+          <use href="src/img/svg/spriteForEachOpt.svg#MyProjectVector"></use>
         </svg>
       </a>
     </div>
